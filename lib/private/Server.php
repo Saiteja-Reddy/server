ff1b11a2b44a5fa5e5818a68a62fbfcd8559ce27
Robin Appelman <icewind@owncloud.com>
2016-08-01 18:27:07 +0200

    basic lockdown logic
    Signed-off-by: Robin Appelman <icewind@owncloud.com>

--- lib/private/Server.php
+++ lib/private/Server.php
@@ -69,6 +69,7 @@ use OC\IntegrityCheck\Helpers\FileAccessHelper;
 use OC\Lock\DBLockingProvider;
 use OC\Lock\MemcacheLockingProvider;
 use OC\Lock\NoopLockingProvider;
+use OC\Lockdown\LockdownManager;
 use OC\Mail\Mailer;
 use OC\Memcache\ArrayCache;
 use OC\Notification\Manager;
@@ -788,6 +789,9 @@ class Server extends ServerContainer implements IServerContainer {
 				$c->getRootFolder(),
 				$c->getSystemConfig()
 			);
+		});
+		$this->registerService('LockdownManager', function (Server $c) {
+			return new LockdownManager();
 		});
 	}
 
@@ -1527,5 +1531,12 @@ class Server extends ServerContainer implements IServerContainer {
 		/** @var \OC\Files\AppData\Factory $factory */
 		$factory = $this->query(\OC\Files\AppData\Factory::class);
 		return $factory->get($app);
+	}
+
+	/**
+	 * @return \OCP\Lockdown\ILockdownManager
+	 */
+	public function getLockdownManager() {
+		return $this->query('LockdownManager');
 	}
 }
